91cd6946ecd888a69f1607e6c2ad77ca61c9a8e8
life-tushar1 <tusharsharma@Tushars-MacBook-Pro.local>
2017-09-04 20:13:30 +0530

    using GPIO port 11 for PIR sensor detection and GPIO port 12 for flame sensor here we are using conn and conn1 variable to check if connection is lost ,if it is lost the recording will done in local once the the connection is back it will push all the notification and mail according to fire detection or intrusion to remote user

--- security/main.py
+++ security/main.py
@@ -1,23 +1,55 @@
 import time
+import RPi.GPIO as GPIO
 from intrution import intrution
 from flame import flame
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(11,GPIO.IN)
+GPIO.setup(12,GPIO.IN)
 flm=flame()
 intru=intrution()
-#1 is true for pir sensor
 try:
+ conn=0
+ conn1=0
  while True:
-    x=int(input())
-    y=int(input())
-    print x
-    if(x==1):
+
+    if(GPIO.input(11)==1):
         try:
-         intru.start()
+          print "on"
+
+          intru.start()
         except:
             print("connection not found")
-    if(y==1):
+            conn=1
+
+    if(GPIO.input(12)==0):
+         print "flame on babe"
+         try:
+          flm.start()
+         except:
+             print"no connection"
+             conn1=1
+    if(conn1==1):
         try:
-         flm.start()
+            print"trying to connect"
+            flm.start()
+            conn1=0
+
         except:
-            print("connection not found")
+            print "still no connection"
+
+    if(conn==1):
+        try:
+            print"trying to connect"
+            intru.notifi_again()
+            conn=0
+
+        except:
+            print "still no connection"
+
+
+
+
+
 except:
     print("key board intrupt")
